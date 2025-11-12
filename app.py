9164cba8a8c769ea40c69c6e9134ab8c562d6221
cmendes7 <cauamendesalves@gmail.com>
2025-11-12 15:01:09 -0300

    criação de csv de terreno e atualização e projetos

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv("preco_imovel.csv")
+df = pd.read_csv("hectare_terreno.csv")
 
 modelo = LinearRegression()
-x = df[["metros_quadrados"]]
+x = df[["hectare"]]
 y = df[["preco"]]
 modelo.fit(x,y)
 
-st.title("prevendo valor de imovel")
+st.title("prevendo valor de hectares")
 st.divider()
 
-metros_quadrados = st.number_input("digite os metros quadrados")
+hectare_terreno = st.number_input("digite a quantidade de hectares")
 
-if metros_quadrados:
-    preco_previsto = modelo.predict([[metros_quadrados]])[0][0]
-    st.write(f'o valor do apartamento de {metros_quadrados:.2f} m² é de, R$ {preco_previsto:.2f}')
+if hectare_terreno:
+    preco_previsto = modelo.predict([[hectare_terreno]])[0][0]
+    st.write(f'o valor do hectare {hectare_terreno:.2f} é de, R$ {preco_previsto:.2f}')
